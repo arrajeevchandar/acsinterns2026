5a8437cd01f4f96db2b191b78c09cd960069308c
Yanish <yanish400@gmail.com>
2026-05-10 11:33:09 +0530

    Teams page inclusion in home page

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import ScrollToTop from './components/ScrollToTop';
 import Home from './pages/Home';
 import './components/HomeSections.css';
 import Projects from './pages/Projects';
+import Teams from './pages/Teams/Teams';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
+        <Route path="/teams" element={<Teams />} />
       </Routes>
     </div>
   );
