eaf2a3ce752e0160ae3880ab3651a23f2493fa2f
Daniel Paul <danielpaul@adobe.com>
2026-05-11 10:25:41 +0530

    Integrated main + restored Zenith AI chatbot + added FAQs page

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import ScrollToTop from './components/ScrollToTop';
 import Home from './pages/Home';
+import FAQ from './pages/FAQ';
 import './components/HomeSections.css';
 import Projects from './pages/Projects';
 import Teams from './pages/Teams/Teams';
@@ -18,6 +19,7 @@ function App() {
         <Route path="/home" element={<Home />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/teams" element={<Teams />} />
+        <Route path="/faqs" element={<FAQ />} />
       </Routes>
     </div>
   );
